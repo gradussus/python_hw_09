1f24f6b3aaf20a75cd733526a6fa588e414c3b22
unknown <shevchenkovitaliy20@gmail.com>
2023-07-13 08:44:14 +0300

    done

--- main.py
+++ main.py
@@ -1,33 +1,71 @@
-contacts = {'Duda': '+3806666666666', 'Budanov': '+380999999999'}
+from input_error import input_error
+
+def hello_handler():
+    return "How can I help you?"
+
+def close_handler():
+    return "Good bye!"
+
+@input_error
+def add_handler(contact):
+    name, phone = contact.split(' ')
+    phonebook[name] = phone
+    return "Contact added successfully"
+
+@input_error
+def change_handler(contact):
+    name, phone = contact.split(' ')
+    phonebook[name] = phone
+    return "Phone number changed successfully"
+
+@input_error
+def phone_handler(name):
+    return phonebook[name]
+
+def show_all_handler():
+    if not phonebook:
+        return "No contacts found"
+    contacts = []
+    for name, phone in phonebook.items():
+        contacts.append(f"{name}: {phone}")
+    return '\n'.join(contacts)
 
 
 def main():
     while True:
         
         request = input().lower()
+
+
         if request == "good bye" or request == "close" or request == "exit":
-            print('Good bye!')
+            print(close_handler())
             break
+
         elif request == "hello":
-            print("How can I help you?")
+            print(hello_handler())
+
         elif request == 'show all':
-            print('show all')
+            print(show_all_handler())
         
-        if request.split(' ')[0] == 'add':
-            print('add')
-        if request.split(' ')[0] == 'change':
-            print('change')
-        if request.split(' ')[0] == 'phone':
-            number = find_phone_by_name(contacts, request.split(' ')[1])
+        elif request.split(' ')[0] == 'add':
+            result = add_handler(request[4:])
+            print(result)
+
+        elif request.split(' ')[0] == 'change':
+            result = change_handler(request[7:])
+            print(result)
+
+        elif request.split(' ')[0] == 'phone':
+            number = phone_handler(request[6:])
             print(number)
 
+        else:
+            print('I do not understand you')
+
         
-def find_phone_by_name(contacts, name):
-    for i in contacts:
-        if i.lower() == name:
-            print(contacts[i])
+
 
 
 if __name__ == '__main__':
-
+    phonebook = {}
     main()
